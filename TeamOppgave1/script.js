45d14044b5963e1d2d94493fc838dc46c5324f0c
RemyDale <remydale@hotmail.com>
2026-01-28 14:37:43 +0100

    Div JS

---
+++ TeamOppgave1/script.js
@@ -0,0 +1,64 @@
+
+//Buttons
+function noLayout(){
+
+}
+function verticalLayout(){
+
+}
+function horizontalLayout(){
+
+}
+function gridLayout(){
+
+}
+function showBodyGame(){
+
+}
+
+
+
+
+//Cards
+function clearAll(){
+    document.querySelector("#card-1 >.content").innerHTML="";
+    document.querySelector("#card-2 >.content").innerHTML="";
+    document.querySelector("#card-3 >.content").innerHTML="";
+    document.querySelector("#card-4 >.content").innerHTML="";
+    document.querySelector("#card-5 >.content").innerHTML="";
+}
+
+function showTools(){
+    clearAll()
+    document.querySelector("#card-1 >.content").innerHTML="";
+     <div class="innerCard">
+        De to viktigste verktøyene vi skal bruke er disse:
+            <ul>
+                <li>
+                    Koderedigeringsprogrammet <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+                    <br />Vi skal bruke noen <i>extensions</i>:
+                    <ul>
+                        <li>JavaScript-booster</li>
+                        <li>es6-string-html</li>
+                        <li>live-server</li>
+                        <li>live-share</li>
+                    </ul>
+                </li>
+                <li>Nettleseren <a href="https://www.google.com/intl/no/chrome/">Google Chrome</a></li>
+            </ul>        
+        </div>
+}
+
+function showHtml(){
+
+}
+function showCss(){
+
+}
+function showJS(){
+
+}
+function showBodyGame(){
+
+}
+
